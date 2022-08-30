be67650638a26a663f71c8ebeee9d46fa1c9f2f3
Brian Pho <brianpho7@gmail.com>
2022-08-30 10:22:46 -0600

    Fix client bug (importing react in index.js)

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
